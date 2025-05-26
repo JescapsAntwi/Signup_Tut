22a193307f349f589c901439c227c5a34e6b0e0b
JescapsAntwi <antwijescaps1@gmail.com>
2025-05-26 16:31:16 +0000

    Minor Changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-//this is the main componene
+//this is the main component
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
